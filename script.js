2881b0da1786b6fa18939740849abb667be468be
Karl Vestin <rixw@me.com>
2025-01-05 09:44:05 -0500

    cells in grid appear and dissapear on hover

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
 let gridContainer = document.querySelector(".grid-container")
+let hoveredColor = document.querySelector(".hovered-color")
 
 function createDiv() {
     let div = document.createElement("div");
     div.classList.add("grid__div");
     let color = `hsl(${Math.floor(Math.random() * 360 + 1)}, 65%, 75%)`;
-    console.log(color);
+    // console.log(color);
     div.style.backgroundColor = color;
     return div
 }
 
-
-
 for (let i = 0; i < 256; i++) {
     gridContainer.appendChild(createDiv())
 }
+
+let allDivs = Array.from(document.querySelectorAll(".grid__div"));
+
+// console.log(allDivs);
+
+let orgColor
+let orgColorArray = [];
+
+function addHoverOnDiv() {
+    console.log(this);
+    let thisDiv = this;
+
+    // let currentOpacity = parseInt(getComputedStyle(this).opacity);
+    // console.log(currentOpacity);
+    // let newOp = currentOpacity + 0.5;
+    // console.log(newOp);
+
+    // // this.style.opacity = currentOpacity + Math.min(0.1, 1);
+    // setTimeout(() => {
+    //     thisDiv.style.opacity += currentOpacity + 0.5;
+ 
+    // }, 100);
+    if (this.style.opacity == 0) {
+        this.style.opacity = 1;
+    } else {
+        this.style.opacity = 0;
+    }
+}
+
+function removeHoverOnDiv() {
+    setTimeout(() => {
+        this.style.backgroundColor = orgColorArray.shift();
+    }, 500);
+}
+
+
+
+allDivs.forEach(div => {
+    div.addEventListener('mouseenter', addHoverOnDiv);
+    // div.addEventListener('mouseleave', removeHoverOnDiv);
+})
