2d56f53e4454443936ef97a4c93d1933b3e5df7b
Karl Vestin <rixw@me.com>
2025-01-04 20:26:19 -0500

    grid container and js created grids implemented

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello");
+let gridContainer = document.querySelector(".grid-container")
+
+function createDiv() {
+    let div = document.createElement("div");
+    div.classList.add("grid__div");
+    let color = `hsl(${Math.floor(Math.random() * 360 + 1)}, 65%, 75%)`;
+    console.log(color);
+    div.style.backgroundColor = color;
+    return div
+}
+
+
+
+for (let i = 0; i < 256; i++) {
+    gridContainer.appendChild(createDiv())
+}
