3f80d865d9dcac871010af2969ea834865e4fff6
Karl Vestin <rixw@me.com>
2025-01-07 10:55:45 -0500

    skratch opacity  on touch fixed

--- script.js
+++ script.js
@@ -49,15 +49,15 @@ pixelButton.addEventListener('click', () => {
 
 
 
-function addHoverOnDiv(event, pixelsWide) {
-    let hoveredDiv = event.srcElement
-    let divIndex = allDivs.indexOf(hoveredDiv);
+function addHoverOnDiv(target, pixelsWide) {
+    let targetDiv = target;
+    let divIndex = allDivs.indexOf(targetDiv);
     square = pixelsWide * pixelsWide;
 
     let centerErase = 0.4;
     let surroundingErase = 0.2;
 
-    let currentOpacity = Number(getComputedStyle(hoveredDiv).opacity);
+    let currentOpacity = Number(getComputedStyle(targetDiv).opacity);
     let newOpacity = currentOpacity - centerErase;
     allDivs[divIndex].style.opacity = newOpacity > 0 ? newOpacity : 0;
 
@@ -65,7 +65,7 @@ function addHoverOnDiv(event, pixelsWide) {
 
     if (divIndex - pixelsWide >= 0) surroundingDivs.push(allDivs[divIndex - pixelsWide])
     if (divIndex + pixelsWide < square) surroundingDivs.push(allDivs[divIndex + pixelsWide])
-        console.log(surroundingDivs);
+    // console.log(surroundingDivs);
 
 
     if (divIndex % pixelsWide === 0) {
@@ -76,7 +76,7 @@ function addHoverOnDiv(event, pixelsWide) {
         surroundingDivs.push(allDivs[divIndex + 1]);
         surroundingDivs.push(allDivs[divIndex - 1]);
     }
-    console.log(surroundingDivs);
+    // console.log(surroundingDivs);
 
     surroundingDivs.forEach(div => {
         let edgeOpacity = Number(getComputedStyle(div).opacity);
@@ -108,37 +108,74 @@ function throttle(func, limit) {
 }
 
 // Throttle usage
-const throttledMove = throttle((e, pixelsWide) => {
+const throttledMove = throttle((target, pixelsWide) => {
     // console.log(activeDivIndex);
-    const targetDiv = e.target;
+    let targetDiv = target;
 
     if (isDragging && allDivs.indexOf(targetDiv) !== activeDivIndex) {
+        console.log(allDivs.indexOf(targetDiv), activeDivIndex);
         activeDivIndex = allDivs.indexOf(targetDiv);
         // console.log(activeDivIndex);
 
-        addHoverOnDiv(e, pixelsWide);
-    }}, 100); // Fires at most once every 200ms
+        addHoverOnDiv(target, pixelsWide);
+    }
+}, 100); // Fires at most once every 200ms
 
 
 function createHoverEffect(pixelsWide, allDivs) {
     allDivs.forEach(div => {
-    div.addEventListener('mousedown', (e) => {
-        activeDivIndex = allDivs.indexOf(e.target);
-        isDragging = true;
-        addHoverOnDiv(e, pixelsWide)
-    });
-    div.addEventListener('mouseup', () => (isDragging = false));
-    div.addEventListener('mousemove', (e) => throttledMove(e, pixelsWide));
+        div.addEventListener('mousedown', (e) => {
+            isDragging = true;
+            let target = e.target;
+            activeDivIndex = allDivs.indexOf(target);
+            console.log(target);
+            addHoverOnDiv(target, pixelsWide)
+        });
+        div.addEventListener('mouseup', () => (isDragging = false));
+        div.addEventListener('mousemove', (e) => {
+            let target = e.target;
+            // console.log(target);
+            throttledMove(target, pixelsWide);
+        })
+        div.addEventListener('touchstart', (e) => {
+            isDragging = true;
 
-    div.addEventListener('touchstart', (e) => {
-        activeDivIndex = allDivs.indexOf(e.target);
-        isDragging = true;
-        addHoverOnDiv(e, pixelsWide);
-    });
-    div.addEventListener('touchend', () => (isDragging = false));
-    div.addEventListener('touchmove', (e) => throttledMove(e, pixelsWide));
+            let target = e.target;
+            activeDivIndex = allDivs.indexOf(target);
+            addHoverOnDiv(target, pixelsWide);
+        });
 
-})
+        div.addEventListener('touchmove', (e) => {
+            // e.preventDefault(); // Prevent scrolling
+            isDragging = true;
+            const touch = e.touches[0];
+            const target = document.elementFromPoint(touch.clientX, touch.clientY);
+            throttledMove(target, pixelsWide)
+
+            // if (lastTouchTarget !== target) {
+            //     console.log("yes");
+            //     lastTouchTarget = target;
+            //     // activeDivIndex = allDivs.indexOf(target);
+            //     throttledMove(target, pixelsWide)
+            // } else {
+            //     console.log("no");
+            //     throttledMove(lastTouchTarget, pixelsWide)
+
+            // }
+
+
+
+            // if (target && target.classList.contains('grid__div')) {
+            // target.classList.add('hover');
+            // console.log(target);
+            // }
+        });
+        div.addEventListener('touchend', () => (isDragging = false));
+        // div.addEventListener('touchmove', (e) => throttledMove(e, pixelsWide));
+
+
+
+    })
 }
 
 createHoverEffect(pixelsWide, allDivs)
